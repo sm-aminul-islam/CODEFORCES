e4004eb1434fb43ffa05ce851e2049d9211677d9
sm-aminul-islam <71812324+sm-aminul-islam@users.noreply.github.com>
2021-12-24 20:31:15 +0600

    Create A - In Search of an Easy Problem.java

---
+++ A - In Search of an Easy Problem.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            int d=sc.nextInt();
+            if(d==1)
+            {
+                System.out.println("HARD");
+                return;
+            }
+
+            }
+        System.out.println("EASY");
+    }
+    }
+
