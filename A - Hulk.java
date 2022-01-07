e1cc41bda129584426a8b4a0c9d8186b31a319d6
sm-aminul-islam <71812324+sm-aminul-islam@users.noreply.github.com>
2022-01-07 20:45:00 +0600

    Create A - Hulk.java

---
+++ A - Hulk.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Hulk {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+       int n=sc.nextInt();
+       for(int i=1;i<=n;i++) {
+        if(i%2==0)
+        {
+            System.out.print("I love ");
+        }
+        else {
+            System.out.print("I hate ");
+        }
+        if(i==n)
+        {
+            System.out.print("it");
+        }
+        else {
+            System.out.print("that ");
+        }
+       }
+    }
+}
