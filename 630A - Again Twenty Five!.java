85643fd9ff532ea050699fb164dc53d079dfd435
sm-aminul-islam <71812324+sm-aminul-islam@users.noreply.github.com>
2022-01-24 15:14:35 +0600

    Create 630A - Again Twenty Five!.java

---
+++ 630A - Again Twenty Five!.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class TwentyFive {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        System.out.println("25");
+    }
+}
