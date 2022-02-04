a5a2ad6ea26e7486d5ff3b6e8495013773c21579
sm-aminul-islam <71812324+sm-aminul-islam@users.noreply.github.com>
2022-02-05 01:28:37 +0600

    Create 1186A - Vus the Cossack and a Contest.java

---
+++ 1186A - Vus the Cossack and a Contest.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Cossackcontest {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),m=sc.nextInt(),k=sc.nextInt();
+        if(m>=n && k>=n){
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+    }
+}
