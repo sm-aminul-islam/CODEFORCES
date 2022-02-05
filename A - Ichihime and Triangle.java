4f7f381c6c03a8f00d412a1514a4fb6f08ffbd4b
sm-aminul-islam <71812324+sm-aminul-islam@users.noreply.github.com>
2022-02-05 16:01:30 +0600

    Create A - Ichihime and Triangle.java

---
+++ A - Ichihime and Triangle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Opposite {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+       for(int i=0;i<t;i++){
+           int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt(),d=sc.nextInt();
+           System.out.println(b+" "+c+" "+c);
+       }
+        }
+    }
+
