3b3b2dd68a8e170785cb3edfaaa12bcbeba779f8
sm-aminul-islam <71812324+sm-aminul-islam@users.noreply.github.com>
2022-02-07 00:23:48 +0600

    Create 1370A - Maximum GCD.java

---
+++ 1370A - Maximum GCD.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class CodeForces {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+            int n=sc.nextInt();
+            System.out.println((int)n/2);
+        }
+    }
+}
