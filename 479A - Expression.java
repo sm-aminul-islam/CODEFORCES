39176d78dc5913a663bb00c72943165b07168def
sm-aminul-islam <71812324+sm-aminul-islam@users.noreply.github.com>
2022-03-16 18:35:06 +0600

    Create 479A - Expression.java

---
+++ 479A - Expression.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Expression {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt();
+        int ans=a+b+c;
+        ans=Math.max(ans,(a+b)*c);
+        ans=Math.max(ans,a*(b+c));
+        ans=Math.max(ans,a*b*c);
+        System.out.println(ans);
+    }
+}
