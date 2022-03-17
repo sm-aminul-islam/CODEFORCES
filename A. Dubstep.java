f700734cb3f9227d3c7ff3981c1306c608a1feff
sm-aminul-islam <71812324+sm-aminul-islam@users.noreply.github.com>
2022-03-18 03:08:10 +0600

    Create A. Dubstep.java

---
+++ A. Dubstep.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Dubstep {
+    public static void main(String[] args) {
+        Scanner  sc=new Scanner(System.in);
+        String s=sc.next();
+        String word="WUB";
+         s=s.replaceAll("WUB"," ");
+         s=s.trim();
+        System.out.println(s);
+
+    }
+
+
+}
