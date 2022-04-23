b63335b4a62dde2faef0d7e7d2975b720abcedf8
sm-aminul-islam <71812324+sm-aminul-islam@users.noreply.github.com>
2022-04-23 15:22:01 +0600

    Create 1325A - EhAb AnD gCd.java

---
+++ 1325A - EhAb AnD gCd.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author devil
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int x=sc.nextInt();
+            System.out.println("1"+" "+(x-1));
+        }
+    }
+}
