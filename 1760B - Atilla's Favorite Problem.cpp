b79dea63b665a052ab0c8626d34ccbd8be94c98f
S.M.Aminul Islam((Robin) <71812324+sm-aminul-islam@users.noreply.github.com>
2022-12-01 21:56:11 +0600

    Create 1760B - Atilla's Favorite Problem.cpp

---
+++ 1760B - Atilla's Favorite Problem.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;  cin>>n;
+        string s; cin>>s;
+       char c= *max_element(s.begin(),s.end());
+       cout<<c-'a'+1<<endl;
+
+
+    }
+    return 0;
+}
